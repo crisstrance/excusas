ed4758c659a21bdc36762873e64b1daa78acc893
crisstrance <50273105+crisstrance@users.noreply.github.com>
2024-07-01 18:35:29 +0000

    excusas-cris

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  generateExcuse();
+});
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let who = ["Mi perro", "Mi abuela", "El cartero", "Mi pajarito"];
+  let action = ["comió", "meó", "rompió", "estalló"];
+  let what = ["mi tarea", "mi móvil", "el auto"];
+  let when = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicios",
+    "durante el almuerzo",
+    "cuando miraba series",
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  document.getElementById("excuse").innerHTML = excuse;
+}
